f78ff91907708058a95d1b7f9567b2bcc2d4fa14
theBowja <ericxin@gmail.com>
2024-01-21 02:00:04 -0500

    fix tcg share code

--- api/tcgdeckshare/decode.mjs
+++ api/tcgdeckshare/decode.mjs
@@ -6,25 +6,27 @@ export default function fetchUser(req, res) {
 	if (!enableCors(req, res)) return;
 	if (req.query.code === undefined) return;
 
-	let code = decodeURIComponent(req.query.code.replaceAll(' ', '+'));
+	const code = decodeURIComponent(req.query.code.replaceAll(' ', '+'));
 
-	let output = decode(code);
+	const output = decode(code);
 
-	return res.json({ deckcode: code, cardshareids: output });
+	return res.json({ deckcode: code, offset: output.offset, cardshareids: output.cards });
 }
 
 function decode(str) {
 	const byteArray = Array.from(atob(str), c => c.codePointAt(0));
+
 	const lastByte = byteArray.pop();
 
 	const reordered = [
-		...Array.from({ length: 25 }).fill(0).map((_, i) => byteArray[2 * i] - lastByte),
-		...Array.from({ length: 25 }).fill(0).map((_, i) => byteArray[2 * i + 1] - lastByte),
+		...Array.from({ length: 25 }).map((_, i) => (byteArray[2 * i] - lastByte) & 255),
+		...Array.from({ length: 25 }).map((_, i) => (byteArray[2 * i + 1] - lastByte) & 255),
 		0,
 	];
 
 	const output = convertXtoYbitarray(8, 12, reordered);
+
 	output.pop();
 
-	return output;
+	return { cards: output, lastByte: lastByte };
 }
--- api/tcgdeckshare/encode.mjs
+++ api/tcgdeckshare/encode.mjs
@@ -1,33 +1,31 @@
 import convertXtoYbitarray from "convert-x-to-y-bit-array";
 import { getData, enableCors } from '../../main.js';
 
-// `/api/tcgdeckshare/decode
+// `/api/tcgdeckshare/encode
 export default function fetchUser(req, res) {
 	if (!enableCors(req, res)) return;
 	if (req.query.deck === undefined) return;
 
-	let deck = req.query.deck.split(',').map(id => parseInt(id));
+	const deck = req.query.deck.split(',').map(id => parseInt(id));
+	const offset = req.query.offset || 0;
 
-	let output = encode(deck);
+	const output = encode(deck, offset);
 
-	return res.json({ deckcode: output, cardshareids: deck });
+	return res.json({ deckcode: output, offset: offset, cardshareids: deck });
 }
 
-function encode(deck) {
+function encode(deck, offset=0) {
 	deck.push(0);
 
 	const byteArray = convertXtoYbitarray(12, 8, deck);
 
-	const lastByte = 0;
+	const lastByte = offset;
 
-    const original = Array.from({ length: 25 })
-      .fill(0)
-      .flatMap((_, i) => [byteArray[i] + lastByte, byteArray[i + 25] + lastByte]);
+	const original = Array.from({ length: 25 })
+	  .fill(0)
+	  .flatMap((_, i) => [(byteArray[i] + lastByte) & 255, (byteArray[i + 25] + lastByte) & 255]);
 
-    const shareCode = btoa(String.fromCodePoint(...original, lastByte));
+	const shareCode = btoa(String.fromCodePoint(...original, lastByte));
 
 	return shareCode;
 }
-
-// let tmp = encode([277,286,287,296,151,151,201,201,205,205,217,217,222,241,241,242,194,243,244,244,245,245,246,246,253,253,255,255,266,267,267,270,256]);
-// console.log(tmp);
